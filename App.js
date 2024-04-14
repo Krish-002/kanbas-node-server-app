45cc3a48109349041a2face602a13e7977f65338
Krish Bansal <73291597+Krish-002@users.noreply.github.com>
2024-04-14 05:32:14 -0400

    a6 p1

--- App.js
+++ App.js
@@ -1,13 +1,38 @@
 import express from "express";
+import mongoose from "mongoose";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5.js";
 import cors from "cors";
-import CourseRoutes from "./Kanbas/courses/routes.js";
+import CourseRoutes from "./Courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
+import UserRoutes from "./Users/routes.js";
+import session from "express-session";
+import "dotenv/config";
 
 
+mongoose.connect("mongodb+srv://krish:mongo@cluster79962.trmwwmq.mongodb.net/kanbas");
 const app = express();
-app.use(cors());
+app.use(
+    cors({
+        credentials: true,
+        origin: process.env.FRONTEND_URL
+    })
+);
+const sessionOptions = {
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  };
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+      domain: process.env.HTTP_SERVER_DOMAIN,
+    };
+  }
+  app.use(session(sessionOptions));
+  
 app.use(express.json());
 
 
@@ -15,4 +40,5 @@ Lab5(app);
 Hello(app);
 CourseRoutes(app);
 ModuleRoutes(app);
+UserRoutes(app);
 app.listen(process.env.PORT || 4000)
---
+++ Courses/routes.js
@@ -0,0 +1,46 @@
+import * as dao from "./dao.js";
+
+export default function CourseRoutes(app) {
+    const createCourse = async (req, res) => {
+        const course = await dao.createCourse(req.body);
+        res.json(course);
+    };
+    const deleteCourse = async (req, res) => {
+        const status = await dao.deleteCourse(req.params.courseId);
+        res.json(status);
+    };
+    const findAllCourses = async (req, res) => {
+        const courses = await dao.findAllCourses();
+        res.json(courses);
+    };
+    
+    const findCourseById = async (req, res) => { 
+        const course = await dao.findCourseById(req.params.courseId);
+        res.json(course);
+    };
+
+    const findCourseByNumber = async (req, res) => { 
+        const course = await dao.findCourseByNumber(req.params.number);
+        res.json(course);
+    };
+
+    const findCourseByCustomId = async (req, res) => { 
+        const course = await dao.findCourseByCustomId(req.params.customId);
+        res.json(course);
+    };
+
+    const updateCourse = async (req, res) => {
+        const { courseId } = req.params;
+        const status = await dao.updateCourse(courseId, req.body);
+        const currentCourse = await dao.findCourseById(courseId);
+        res.json(status);
+    };
+
+    app.post("/api/courses", createCourse);
+    app.get("/api/courses", findAllCourses);
+    app.get("/api/courses/:courseId", findCourseById);
+    app.get("/api/courses/:customId", findCourseByCustomId);
+    app.get("/api/courses/:number", findCourseByNumber);
+    app.put("/api/courses/:courseId", updateCourse);
+    app.delete("/api/courses/:courseId", deleteCourse);
+}
